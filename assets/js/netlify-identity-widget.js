600a0d25b06a021f2fe36142c47a714dbec53adf
Matija Micheli <127015629+MatijaMicheli@users.noreply.github.com>
2025-06-04 22:33:42 +1200

    Add files via upload

--- assets/js/netlify-identity-widget.js
+++ assets/js/netlify-identity-widget.js
@@ -1,2 +1,14 @@
-// Placeholder for netlify-identity-widget.js
-console.log('Netlify Identity loaded');
+<script src="/assets/js/netlify-identity-widget.js"></script>
+<script>
+  if (window.netlifyIdentity) {
+    window.netlifyIdentity.init();
+
+    window.netlifyIdentity.on("init", (user) => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        });
+      }
+    });
+  }
+</script>
